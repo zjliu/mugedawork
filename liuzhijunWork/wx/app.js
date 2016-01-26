830d318592d615e0ab4e620d06b24d8b8785aa54
zjliu <liuzhijunsw@126.com>
2016-01-26 17:46:26 +0800

    remove url

--- liuzhijunWork/wx/app.js
+++ liuzhijunWork/wx/app.js
@@ -2,9 +2,9 @@ var express=require('express');
 var app=express();
 var jsSHA = require("jssha");
 var nodegrass = require('nodegrass');
-function getSignature(appid,secret,url,callback){
-    function calcSignature(ticket, noncestr, ts, url) {
-        var str = 'jsapi_ticket=' + ticket + '&noncestr=' + noncestr + '&timestamp='+ ts +'&url=' + url;
+function getSignature(appid,secret,callback){
+    function calcSignature(ticket, noncestr, ts) {
+        var str = 'jsapi_ticket=' + ticket + '&noncestr=' + noncestr + '&timestamp='+ ts;
         var shaObj = new jsSHA("SHA-1", 'TEXT');
 			shaObj.update(str);
         return shaObj.getHash('HEX');
@@ -23,18 +23,17 @@ function getSignature(appid,secret,url,callback){
         nodegrass.get(ticket_url,function(data,status){
 			if(!status===200){ callback(false); return; }
 			var ticketObj = JSON.parse(data);
-			var reObj = {appId:appid,nonceStr:createNonceStr(),timestamp:createTimeStamp(),url:url};
-				reObj.signature=calcSignature(ticketObj.ticket,reObj.nonceStr,reObj.timestamp,url);
+			var reObj = {appId:appid,nonceStr:createNonceStr(),timestamp:createTimeStamp()};
+				reObj.signature=calcSignature(ticketObj.ticket,reObj.nonceStr,reObj.timestamp);
 			callback(reObj);
         });
     });
 }
 
-app.post('/querySignture',function(req,res){ 
+app.get('/querySignture',function(req,res){ 
 	var appid = 'wx501b4f006c6939be';
 	var secret = 'ba7448218aef98f28f1b95e13374358d';
-	var url = req.body.url;
-	getSignature(appid,secret,url,function(signObj){
+	getSignature(appid,secret,function(signObj){
 		res.json(signObj);
 	});
 });
